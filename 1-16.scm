c4c65338b17277b033d08c842a310c7e1a08cafa
Ryan Selk <ryanselk@gmail.com>
2013-12-17 23:31:12 -0700

    problem 16

---
+++ 1-16.scm
@@ -0,0 +1,15 @@
+;;scip 1-16
+
+
+(define (sqr b n)
+  (iter-square 1 b n))
+
+(define (iter-square a b n)
+  (cond((= n 0) a)
+  ((even? n) 
+    (iter-square a (* b b) (/ n 2)))
+  (else 
+    (iter-square (* a b) b (- n 1)))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
