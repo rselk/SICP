793c9860576a94b7a2d3b9a986bdb212057f3aa7
Ryan Selk <ryanselk@gmail.com>
2014-01-03 00:41:31 -0700

    problem 2-25

---
+++ ch2/2-25.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+;; Exercise 2.25.  Give combinations of cars and cdrs that will
+;; pick 7 from each of the following lists:
+
+(define l1 (list 1 3 (list 5 7) 9))
+(car (cdr (car (cdr (cdr l1)))))
+
+(define l2 (list (list 7)))
+(car (car l2))
+
+(define l3 (list 1 (list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
+(car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr l3))))))))))))
+
