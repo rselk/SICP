2ae95e69cfdb4b3647f1b9a1e7ed13e6b93ceaba
Ryan Selk <ryanselk@gmail.com>
2014-01-06 10:26:03 -0700

    problem 2-34/35

--- ch2/2-35.rkt
+++ ch2/2-35.rkt
@@ -1,13 +1,6 @@
 #lang racket
 
-;; Fill in the following template to produce a procedure that evaluates
-;; a polynomial using Horner's rule. Assume that the coefficients of the
-;; polynomial are arranged in a sequence, from a0 through an.
-
-;; (define (horner-eval x coefficient-sequence)
-;;  (accumulate (lambda (this-coeff higher-terms) <??>)
-;;              0
-;               coefficient-sequence))
+;; Exercise 2.35.  Redefine count-leaves from section 2.2.2 as an accumulation
 
 ;; accumulate function
  (define (accumulate op initial sequence) 
@@ -16,12 +9,29 @@
        (op (car sequence) 
            (accumulate op initial (cdr sequence))))) 
  
- ;; horner evaluation
-(define (horner-eval x coefficient-sequence)
-  (accumulate (lambda (this-coeff higher-terms) 
-                (+ this-coeff (* higher-terms x)))
-              0
-              coefficient-sequence))
+;;original
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
 
-(horner-eval 2 (list 1 3 0 5 0 1))
-;; 79
+;;as a accumulation
+
+;; If its a pair, call it with count-leaves-accumulate as that lambda will break down the pair.
+;; ie (2 3 (4 5) 6) 
+;; the lambda function will return 1 for 2, 3
+;; then it will recursivly call count-leaves-accumulate with (4 5)
+;; the lambda function will return 1 for 4, 5
+;; once that evaluates we will be back in the original function call
+;; the lambda function will return 1 for 6
+;; while doing all of this the accumulate function has been summing all of the 1s returned
+
+(define (count-leaves-accumulate t)
+  (accumulate + 0 (map (lambda (x) (if (pair? x) 
+                                       (count-leaves-accumulate x)
+                                       1))t)))
+;;tests
+(define x (cons (list 1 2) (list 3 4)))
+(count-leaves (list x x)) ;;8
+(count-leaves-accumulate (list x x)) ;;8
