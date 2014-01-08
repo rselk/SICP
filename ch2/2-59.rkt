67d93e1094da91c662ffc8a168c0bfa7e540109d
Ryan Selk <ryanselk@gmail.com>
2014-01-08 16:18:34 -0700

    problem 2-59

--- ch2/2-59.rkt
+++ ch2/2-59.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;; Exercise 2.59.  Implement the union-set operation for the
+;; unordered-list representation of sets.
+
 (define (element-of-set? x set)
   (cond ((null? set) false)
         ((equal? x (car set)) true)
