cc826478a41d1c579639d173dc6422a00bec63da
Ryan Selk <ryanselk@gmail.com>
2014-01-12 13:58:30 -0700

    problem 2-81

--- ch2/2-81.rkt
+++ ch2/2-81.rkt
@@ -12,3 +12,57 @@
 
 ;; B) Nothing is wrong with the code that I can see. Apply-generic should work.
 
+
+;; c. Modify apply-generic so that it doesn't try coercion if the two arguments
+;; have the same type.
+
+;; Original Apply-generic
+;;(define (apply-generic op . args)
+;;  (let ((type-tags (map type-tag args)))
+;;    (let ((proc (get op type-tags)))
+;;      (if proc
+;;          (apply proc (map contents args))
+;;          (if (= (length args) 2)
+;;              (let ((type1 (car type-tags))
+;;                    (type2 (cadr type-tags))
+;;                    (a1 (car args))
+;;                    (a2 (cadr args)))
+;;                (let ((t1->t2 (get-coercion type1 type2))
+;;                      (t2->t1 (get-coercion type2 type1)))
+;;                  (cond (t1->t2
+;;                         (apply-generic op (t1->t2 a1) a2))
+;;                        (t2->t1
+;;                         (apply-generic op a1 (t2->t1 a2)))
+;;                        (else
+;;                         (error "No method for these types"
+;;                                (list op type-tags))))))
+;;              (error "No method for these types"
+;;                     (list op type-tags)))))))
+
+;; modified apply generic
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (if (= (length args) 2)
+              (let ((type1 (car type-tags))
+                    (type2 (cadr type-tags))
+                    (a1 (car args))
+                    (a2 (cadr args)))
+                ;;below is the change, check to see if types are equal.
+                ;;if they are equal we can return an error 
+                (if (equal? type1 type2)
+                    (error "no method" type1)
+                    (let ((t1->t2 (get-coercion type1 type2))
+                          (t2->t1 (get-coercion type2 type1)))
+                      (cond (t1->t2
+                             (apply-generic op (t1->t2 a1) a2))
+                            (t2->t1
+                             (apply-generic op a1 (t2->t1 a2)))
+                            (else
+                             (error "No method for these types"
+                                    (list op type-tags)))))
+                    (error "No method for these types"
+                           (list op type-tags)))))))))
